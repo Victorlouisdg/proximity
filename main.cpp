ba6d9bea6905b08171f494ddad6ab279b09c7c83
victorlouisdg <victorlouisdg@gmail.com>
2023-02-28 11:41:10 +0100

    initial version of the ui

--- main.cpp
+++ main.cpp
@@ -1,34 +1,77 @@
 #include <igl/opengl/glfw/Viewer.h>
+#include <igl/opengl/glfw/imgui/ImGuiMenu.h>
+#include <igl/opengl/glfw/imgui/ImGuiPlugin.h>
+#include <igl/opengl/glfw/imgui/ImGuizmoWidget.h>
+#include <igl/read_triangle_mesh.h>
 
-int main(int argc, char *argv[])
-{
-  // Inline mesh of a cube
-  const Eigen::MatrixXd V= (Eigen::MatrixXd(8,3)<<
-    0.0,0.0,0.0,
-    0.0,0.0,1.0,
-    0.0,1.0,0.0,
-    0.0,1.0,1.0,
-    1.0,0.0,0.0,
-    1.0,0.0,1.0,
-    1.0,1.0,0.0,
-    1.0,1.0,1.0).finished();
-  const Eigen::MatrixXi F = (Eigen::MatrixXi(12,3)<<
-    0,6,4,
-    0,2,6,
-    0,3,2,
-    0,1,3,
-    2,7,6,
-    2,3,7,
-    4,6,7,
-    4,7,5,
-    0,4,5,
-    0,5,1,
-    1,5,7,
-    1,7,3).finished();
 
-  // Plot the mesh
-  igl::opengl::glfw::Viewer viewer;
-  viewer.data().set_mesh(V, F);
-  viewer.data().set_face_based(true);
-  viewer.launch();
+int main(int argc, char *argv[]) {
+  // Load a mesh from file
+  Eigen::MatrixXd V;
+  Eigen::MatrixXi F;
+  igl::read_triangle_mesh(argc > 1 ? argv[1] : "../screwdriver.off", V, F);
+  // Set up viewer
+  igl::opengl::glfw::Viewer vr;
+  vr.data().set_mesh(V, F);
+
+
+  // Add two points to the viewer
+  Eigen::MatrixXd P(2, 3);
+  double distance_between_points = 0.05;
+  double d2 = distance_between_points / 2.0;
+  P << -d2, 0, 0, d2, 0, 0;
+  vr.data().add_points(P, Eigen::RowVector3d(1, 0, 0));
+  vr.data().add_edges(P.row(0), P.row(1), Eigen::RowVector3d(1, 0, 0));
+
+  igl::opengl::glfw::imgui::ImGuiPlugin imgui_plugin;
+  vr.plugins.push_back(&imgui_plugin);
+
+  // Add a 3D gizmo plugin
+  igl::opengl::glfw::imgui::ImGuizmoWidget gizmo;
+  imgui_plugin.widgets.push_back(&gizmo);
+  // Initialize ImGuizmo at mesh centroid
+  gizmo.T.block(0, 3, 3, 1) =
+      0.5 * (V.colwise().maxCoeff() + V.colwise().minCoeff())
+                .transpose()
+                .cast<float>();
+  // Update can be applied relative to this remembered initial transform
+  const Eigen::Matrix4f T0 = gizmo.T;
+  // Attach callback to apply imguizmo's transform to mesh
+  gizmo.callback = [&](const Eigen::Matrix4f &T) {
+    const Eigen::Matrix4d TT = (T * T0.inverse()).cast<double>().transpose();
+    vr.data().set_vertices(
+        (V.rowwise().homogeneous() * TT).rowwise().hnormalized());
+    vr.data().compute_normals();
+  };
+  // Maya-style keyboard shortcuts for operation
+  vr.callback_key_pressed = [&](decltype(vr) &, unsigned int key, int mod) {
+    switch (key) {
+    case ' ':
+      gizmo.visible = !gizmo.visible;
+      return true;
+    case 'W':
+    case 'w':
+      gizmo.operation = ImGuizmo::TRANSLATE;
+      return true;
+    case 'E':
+    case 'e':
+      gizmo.operation = ImGuizmo::ROTATE;
+      return true;
+    case 'R':
+    case 'r':
+      gizmo.operation = ImGuizmo::SCALE;
+      return true;
+    }
+    return false;
+  };
+
+  igl::opengl::glfw::imgui::ImGuiMenu menu;
+  imgui_plugin.widgets.push_back(&menu);
+
+  std::cout << R"(
+W,w  Switch to translate operation
+E,e  Switch to rotate operation
+R,r  Switch to scale operation
+)";
+  vr.launch();
 }
